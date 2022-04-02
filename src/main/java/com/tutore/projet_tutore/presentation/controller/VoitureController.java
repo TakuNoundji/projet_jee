b5b23821944377682eb77dac495846ea7be58a08
unknown <TakuNoundji>
2022-04-02 13:17:44 +0100

    premier commit

---
+++ src/main/java/com/tutore/projet_tutore/presentation/controller/VoitureController.java
@@ -0,0 +1,68 @@
+package com.tutore.projet_tutore.presentation.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VoitureController {
+
+    @GetMapping("/liste_voiture")
+    public String listeVoiture(){
+
+        return "listeVoiture";
+    }
+
+
+    @GetMapping("/inscription")
+    public String inscription(){
+
+        return "inscription";
+    }
+
+    @GetMapping("/ajoutvoiture")
+    public String ajoutvoiture(){
+
+        return "ajoutvoiture";
+    }
+
+    @GetMapping("/reserver")
+    public String reserver(){
+
+        return "reserver";
+    }
+    @GetMapping("/liste1")
+    public String liste1(){
+
+        return "liste1";
+    }
+
+    @GetMapping("/profil")
+    public String profil(){
+
+        return "profil";
+    }
+
+    @GetMapping("/reservations")
+    public String reservations(){
+
+        return "reservations";
+    }
+    @GetMapping("/connexion")
+    public String connexion(){
+
+        return "connexion";
+
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+
+        return "detail";
+    }
+
+
+}
+
+
+
